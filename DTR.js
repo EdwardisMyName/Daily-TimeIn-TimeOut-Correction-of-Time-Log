e9f63bef8811318ea0472aea097d5a6e56ed505d
Edward Fernandez <100471661+EdwardisMyName@users.noreply.github.com>
2022-03-02 10:42:11 +0800

    Add files via upload

---
+++ DTR.js
@@ -0,0 +1,6 @@
+
+<form id="myForm" action="/action_page.php">
+  First name: <input type="text" name="fname"><br>
+  Last name: <input type="text" name="lname"><br><br>
+  <input type="button" onclick="myFunction()" value="Submit form">
+</form>
